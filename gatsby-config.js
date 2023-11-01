2179127ca92694228ed0d859db2cbfd420f4882b
rshwndsz <30742639+rshwndsz@users.noreply.github.com>
2023-11-01 16:06:15 +0000

    Update name

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,15 +9,15 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Russel's Homepage`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Russel Dsouza`,
+      summary: `, MSc Machine Learning @ the University of Birmingham`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
+    description: `A blog to document my musings on AI`,
+    siteUrl: `https://rshwndsz.github.io`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `rshwndsz`,
     },
   },
   plugins: [
@@ -75,14 +75,14 @@ module.exports = {
         `,
         feeds: [
           {
-            serialize: ({ query: { site, allMarkdownRemark } }) => {
+            serialize: ({query: {site, allMarkdownRemark}}) => {
               return allMarkdownRemark.nodes.map(node => {
                 return Object.assign({}, node.frontmatter, {
                   description: node.excerpt,
                   date: node.frontmatter.date,
                   url: site.siteMetadata.siteUrl + node.fields.slug,
                   guid: site.siteMetadata.siteUrl + node.fields.slug,
-                  custom_elements: [{ "content:encoded": node.html }],
+                  custom_elements: [{"content:encoded": node.html}],
                 })
               })
             },
